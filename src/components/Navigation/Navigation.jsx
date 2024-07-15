dfde7530abde729604653e0694b3b33878ab9d8b
Amin394 <amin394@outlook.com>
2024-07-15 19:35:37 +0100

    fixed problem with file names that start with dot

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -8,7 +8,7 @@ function Navigation({ searchWord, setSearchWord }) {
 
   return (
     <div className="header">
-      <img className="header-logo" src="./images/.logo.png" onClick={() => handleSearch(" ")} />
+      <img className="header-logo" src="./images/_logo.png" onClick={() => handleSearch(" ")} />
       <input
         className="header-search" value={searchWord} type="text" placeholder="🔍  Search ..."
         onChange={(event) => handleSearch(event.target.value)} autoFocus
