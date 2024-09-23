5e5a0f8a321069371c111d38b655a2142bc1f5aa
Amin394 <amin394@outlook.com>
2024-09-23 23:51:54 +0100

    implemented caching inside fetch hook

--- src/utilities/useFetch.jsx
+++ src/utilities/useFetch.jsx
@@ -1,26 +1,35 @@
 import { useState, useEffect } from "react";
 
+const cache = {};
+
 function useFetch(url) {
   const [data, setData] = useState(null);
   const [status, setStatus] = useState("loading");
 
   useEffect(() => {
-    let fetchData = async () => {
-      try {
-        const response = await fetch(url);
-        if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
-        if (response.headers.get("content-type") == "text/html") throw new Error("File Not Found!");
-        
-        const data = await response.text();
-        setData(data);
-        setStatus("complete");
-      } catch (error) {
-        console.log(error);
-        setStatus("error");
-      }
-    };
+    if (cache[url]) {
+      setData(cache[url]);
+      setStatus("complete");
+    }
+    else {
+      let fetchData = async () => {
+        try {
+          const response = await fetch(url);
+          if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
+          if (response.headers.get("content-type") == "text/html") throw new Error("File Not Found!");
 
-    fetchData();
+          const data = await response.text();
+          cache[url] = data;
+          setData(data);
+          setStatus("complete");
+        }
+        catch (error) {
+          console.error(error);
+          setStatus("error");
+        }
+      };
+      fetchData();
+    }
   }, [url]);
 
   return { data, status };
