d0c5cf50e9362b2fc6c24030455faa15b8ffef72
Amin394 <amin394@outlook.com>
2025-04-29 00:22:56 +0100

    displayed thumbnail in the article

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -11,15 +11,17 @@ function Article({ blogData, handleSearch }) {
     return (<div className="error article"> <div>&#x2716;</div> Oops! Something went wrong. </div>);
 
   document.title = blogData.title;
-
+  
   return (
-    <div className="article">
-      <h1>{blogData.title}</h1>
+    <div className="article">      
+      <div className="article-image" style={{backgroundImage: `linear-gradient(rgba(245, 239, 230, 0.2), rgba(245, 239, 230, 1)), url(./images/${blogData.image})`}}>
+        <h1>{blogData.title}</h1>
 
-      <div className="article-info">  
-        <span>📘 &nbsp;{blogData.tags[0]}</span>
-        <span>🖊️ &nbsp;{blogData.author}</span>
-        <span>🕓 &nbsp;{blogData.date}</span>
+        <div className="article-info" >  
+          <span>📘 &nbsp;{blogData.tags[0]}</span>
+          <span>🖊️ &nbsp;{blogData.author}</span>
+          <span>🕓 &nbsp;{blogData.date}</span>
+        </div>
       </div>
 
       <Markdown>{data}</Markdown>
